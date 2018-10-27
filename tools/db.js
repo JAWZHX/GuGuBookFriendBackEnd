d1218041b8ec2a3eb74edf61af5ed44cc777df78
JAWZHX <1219251709@qq.com>
2018-10-27 15:53:13 +0800

    1、添加数据库操作文件

--- tools/db.js
+++ tools/db.js
@@ -1,1 +1,40 @@
 const mysql = require('mysql')
+
+/**
+ * 数据库操作文件
+ */
+
+// 读取数据库配置信息
+require('env2')('../.env')
+const {env} = process
+
+// 创建数据库连接池
+const pool = mysql.createPool({
+    host: env.mysqlHost,
+    user: env.user,
+    password: env.password,
+    database: env.database
+})
+
+const query = (sql, values) => {
+    return new Promise((resolve, reject) => {
+        pool.getConnection((err, connection) => {
+            if(err) {
+                reject(err)
+            } else {
+                connection.query(sql, values, (err, rows) => {
+                    if (err) {
+                        reject(err)
+                    } else {
+                        resolve(rows)
+                    }
+                    connection.release()
+                })
+            }
+        })
+    })
+}
+
+module.exports = {
+    query
+}
