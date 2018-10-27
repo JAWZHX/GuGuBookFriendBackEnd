e11282091e5fbd51fc524927210955c40903ed38
JAWZHX <1219251709@qq.com>
2018-10-27 10:42:14 +0800

    1、 添加请求体解析模块koa-bodyparser

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const Koa = require('koa');
 const app = new Koa();
 const debug = require('debug')('gugubookfriendbackend');
 const response = require('./middlewares/response')
+const bodyparser = require('koa-bodyparser')
 const config = require('./config');
 
 // 应用响应处理中间件
 app.use(response)
+
+// 解析请求体
+app.use(bodyparser())
+
+
 
 app.use(async (ctx, next) => {
     ctx.body = 'Hello world';
